8f1da89f404ca7e6717767612f84e9782b658bde
gurpreet <gurpreetbhatia677@gmail.com>
2026-07-19 21:22:04 +0530

    module enhancements

--- outputs.tf
+++ outputs.tf
@@ -17,7 +17,6 @@ output "availability_set_id" {
   description = "id of the availability set where the vms are provisioned."
   value       = "${azurerm_availability_set.vm.id}"
 }
-
 output "vm_location" {
   description = "Azure region where the virtual machine is deployed."
   value       = var.location
